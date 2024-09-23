31d09bbb1f046bf3855effee2ac4543a2f214ed4
John Carlo Torralba <jctorralba@TELEDIRECTPH.LOCAL>
2024-09-23 18:20:47 +0800

    Update function with schema loading

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
 from pyspark.sql import SparkSession
 
-# Initialize Spark session
-spark = SparkSession.builder \
-    .appName("Load Excel to BigQuery") \
-    .config("spark.jars.packages", "com.google.cloud.spark:spark-bigquery-with-dependencies_2.12:0.29.0,com.crealytics:spark-excel_2.12:0.13.5") \
-    .getOrCreate()
+def process_file(event, context):
+    # Initialize Spark session
+    spark = SparkSession.builder \
+        .appName("Load Excel to BigQuery") \
+        .config("spark.jars.packages", "com.google.cloud.spark:spark-bigquery-with-dependencies_2.12:0.29.0,com.crealytics:spark-excel_2.12:0.13.5") \
+        .getOrCreate()
 
-# File path in Google Cloud Storage
-file_path = "gs://project_fit/2024-09-18 16_57-Roster ALO.xlsx"
+    # File path in Google Cloud Storage
+    file_path = f"gs://{event['bucket']}/{event['name']}"
 
-# Read the Excel file using Spark
-df = spark.read \
-    .format("com.crealytics.spark.excel") \
-    .option("useHeader", "true") \
-    .option("inferSchema", "true") \
-    .load(file_path)
+    # Read the Excel file using Spark
+    df = spark.read \
+        .format("com.crealytics.spark.excel") \
+        .option("useHeader", "true") \
+        .option("inferSchema", "true") \
+        .load(file_path)
 
-# Print the inferred schema
-df.printSchema()
+    # Print the inferred schema
+    df.printSchema()
 
-# Show a preview of the data
-df.show()
+    # Show a preview of the data
+    df.show()
 
-# BigQuery table details
-project_id = "tdcxai-data-science"  # Replace with your GCP project ID
-dataset_id = "tdcxai-data-science.project_fit   "  # Replace with your dataset ID
-table_id = "tbl_alo_roster"
+    # BigQuery table details
+    project_id = "tdcxai-data-science"  # Replace with your GCP project ID
+    dataset_id = "project_fit"  # Replace with your dataset ID
+    table_id = "tbl_alo_roster"
 
-# Write data to BigQuery
-df.write \
-    .format("bigquery") \
-    .option("table", f"{project_id}.{dataset_id}.{table_id}") \
-    .mode("append") \
-    .save()
+    # Write data to BigQuery
+    df.write \
+        .format("bigquery") \
+        .option("table", f"{project_id}.{dataset_id}.{table_id}") \
+        .mode("append") \
+        .save()
 
-# Stop the Spark session
-spark.stop()
+    # Stop the Spark session
+    spark.stop()
